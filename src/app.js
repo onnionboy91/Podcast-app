7cbf9a6f37acb390f11ff53f30168f1ed918c7e6
Viktor <onnionboy@mail.ru>
2023-01-09 15:49:21 +0300

    second commit

--- src/app.js
+++ src/app.js
@@ -1,2 +1,31 @@
+import {Question} from './question';
 import './styles.css';
-console.log('App working...');
+import { isValid } from './utils';
+
+const form = document.getElementById('form');
+const input = form.querySelector('#question-input');
+const submitBtn = form.querySelector('#submit');
+
+form.addEventListener('submit', submitFormHandler);
+input.addEventListener('input', () => {
+    submitBtn.dislabed = !isValid(input.value);
+});
+
+function submitFormHandler(event) {
+    event.preventDefault();
+
+    if(isValid(input.value)) {
+        const question = {
+            text: input.value.trim(),
+            date: new Date().toJSON()
+        };
+
+        submitBtn.disabled = true;
+        //Async request to server to save question
+        Question.create(question).then( () => {
+            input.value = '';
+            input.className = '';
+            submitBtn.disabled = false;
+        });  
+    }
+}
---
+++ src/question.js
@@ -0,0 +1,15 @@
+export class Question {
+    static create(question) {
+        return fetch('https://podcast-app-ddb98-default-rtdb.europe-west1.firebasedatabase.app/questions.json', {
+            method: 'POST',
+            body: JSON.stringify(question),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(response => {
+            console.log(response)
+        })
+    }
+}
